8271b20132b32b7ff213b6de6b6294947772713d
Ji hun <jihoonv0221@gmail.com>
2023-09-09 12:34:49 +0900

    week03 chap 02 v0.7

--- weeko3 chap02.py
+++ weeko3 chap02.py
@@ -1,7 +1,17 @@
-a = 9.1
-print(a)
-b = a
-print(b)
-print(type(9.1))
-print(type(9.1) == int)
-print(isinstance(9.1,float))
+x = [1,2,3]
+y = x
+print(x, y)
+y[0] = -11
+print(x, y)
+#dictionary : mutable
+human = {'eye' : [1.0, 0.9], 'bloodtype' : 'ab'}
+print(human['eye'])
+human['eye'][0]= 0.7
+print(human['eye'])
+
+
+#a = 1
+#b = a
+#print(a, b)
+#a = 2
+#print(a, b)
