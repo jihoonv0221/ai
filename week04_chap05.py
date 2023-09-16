b53c1d066e7ee8b7d6891b4718bca52e934e87fe
Ji hun <jihoonv0221@gmail.com>
2023-09-16 11:47:01 +0900

    week 04 chap05 v1.5

---
+++ week04_chap05.py
@@ -0,0 +1,10 @@
+army = """
+우리는 국가와 국민에 충성을 다하는 대한민국 육군이다.
+    "하나". 우리는 자유 민주주의를 수호하며 조국 통일의 역군이 된다.
+    "둘". 우리는 실전과 같은 훈련으로 지상전의 승리자가 된다.
+"""
+#high_school = "Incheon\nScience\nHigh\nSchool"
+#high_school = "Incheon\bScience\bHigh\bSchool"
+high_school = "Incheon \"Science\" High \ School"
+print(high_school)
+print(army)
