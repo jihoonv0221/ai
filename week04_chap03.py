8d79a015414189ceddfcb38a40548856e77bd32b
Ji hun <jihoonv0221@gmail.com>
2023-09-16 10:23:34 +0900

    week 04 chap02 v1.1

--- week04_chap03.py
+++ week04_chap03.py
@@ -5,3 +5,14 @@ b = 0b1010110001110001
 c = 0o126161
 d = 44145
 print(a,b,c,d)
+print(hex(d), bin(d), oct(d), d)
+# A ~ Z(65 ~ 90)
+print(chr(90))
+print(ord('B'))
+#a ~ z(97 ~ 122)
+print(chr(98))
+print(ord('b'))
+print(ord('김'))
+print(chr(0xAE40))
+print(chr(44608))
+print(hex(ord('김')))
