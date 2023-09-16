14378ac299bdf7eb5a8b43f774f44dad3a66e206
Ji hun <jihoonv0221@gmail.com>
2023-09-16 11:04:17 +0900

    week 04 chap02 v1.2

--- week04_chap03.py
+++ week04_chap03.py
@@ -1,18 +1,14 @@
-a = 0xAC71
-#1010 1100 0111 0001(2)
-b = 0b1010110001110001
-#126161(8)
-c = 0o126161
-d = 44145
-print(a,b,c,d)
-print(hex(d), bin(d), oct(d), d)
-# A ~ Z(65 ~ 90)
-print(chr(90))
-print(ord('B'))
-#a ~ z(97 ~ 122)
-print(chr(98))
-print(ord('b'))
-print(ord('김'))
-print(chr(0xAE40))
-print(chr(44608))
-print(hex(ord('김')))
+pi = 3.141592
+pi_e3 = 0.003141592e3
+print(pi, pi_e3)
+print(bool(-19))
+print(bool(1))
+print(bool(1.0e2))
+print(bool(0))
+print(bool(0.0))
+print(int('F', 16))
+print(int('99'))
+print(float('99.71'))
+#print(int('10e5'))
+print(float('1.0e5'))
+print(True+2)
