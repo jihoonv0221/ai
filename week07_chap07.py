828a785c8520c67af050b3ae23c7889d81918bd8
Ji hun <jihoonv0221@gmail.com>
2023-10-28 11:32:36 +0900

    week07_chap07 v3.5

--- week07_chap07.py
+++ week07_chap07.py
@@ -1,22 +1,32 @@
-numbers = list()
-for i in range(0, 11):
-   #print(i, end=' ')
-    numbers.append(i)
-print(numbers)
-odd_numbers = numbers[1::2]
-even_numbers = numbers[::2]
-print(odd_numbers)
-print(even_numbers)
-reverse_odd_numbers = numbers[-2::1-2]
-print(reverse_odd_numbers)
-reverse_numbers = numbers[::-1]
-numbers.reverse()
-print(reverse_numbers)
-numbers.append(-11)
-print(numbers)
-numbers.insert(5,99)
-print(numbers)
-numbers.insert(-2, 55)
-print(numbers)
-numbers = numbers * 2 # list multiplication
-print(numbers)
+trolly = ['김광석CD', '아이폰15 프로']
+print(trolly)
+food = ['다시마', '멸치']
+trolly.append(food)
+print(trolly)
+print(trolly[2][1])
+#print(trolly.pop()) # 리스트의 맨 마지막 원소를 리턴 후 삭제
+#print(trolly[2].pop()) # 멸치 원소를 리턴 후 삭제
+print(trolly[2].pop(0)) # 다시마 원소를 리턴 후 삭제
+print(trolly)
+# trolly.append('다시마')
+trolly[2].append('다시마')
+print(trolly)
+trolly[2].insert(0, '딸기')
+print(trolly)
+
+# trolly[1:2] = '아이폰 15' # ['김광석CD', '아', '이', '폰', ' ', '1', '5', ['딸기', '멸치', '다시마']]
+
+trolly[1:2] = ['아이폰 15'] #아이폰 15 프로 --> 아이폰 15
+print(trolly)
+
+
+#numbers1 = list()
+#numbers2 = [-9, 0, 7]
+#for i in range(0, 5):
+    #numbers1.append(i)
+#print(numbers1)
+#numbers3 = numbers1 + numbers2
+#print(numbers3)
+#print(numbers1)
+#numbers1.extend(numbers2) # numbers1 = numbers1 + numbers2
+#print(numbers1)
