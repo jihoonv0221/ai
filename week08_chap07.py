5b8f0dfbb47889b80f2ade3680239f2e3a828db5
Ji hun <jihoonv0221@gmail.com>
2023-11-04 12:18:24 +0900

    week08_chap07 v4.3

--- week08_chap07.py
+++ week08_chap07.py
@@ -1,9 +1,7 @@
+import copy
 a = [-11, [3, 0], 9]
-b = a.copy() # shallow copy
-c = list(a) # shallow copy
-d = a[:] # shallow copy
-e = a
+b = copy.deepcopy(a)  # deep copy
 
-print(a, b, c, d, e)
+print(a,b)
 a[1][0] = 7
-print(a, b, c, d, e)
+print(a,b)  # 깊은 복사 리스트 b는 변하지 않음
