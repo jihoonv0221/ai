dd723ddc9aa221986bfe48feed82528d20769e3f
Ji hun <jihoonv0221@gmail.com>
2023-11-04 12:16:18 +0900

    week08_chap07 v4.2

--- week08_chap07.py
+++ week08_chap07.py
@@ -1,11 +1,9 @@
-a = [-11, 72, 9]
+a = [-11, [3, 0], 9]
 b = a.copy() # shallow copy
 c = list(a) # shallow copy
 d = a[:] # shallow copy
 e = a
 
 print(a, b, c, d, e)
-a[1] = 2.7
-print(a, b, c, d, e)
-e[0] = 'ishs'
+a[1][0] = 7
 print(a, b, c, d, e)
