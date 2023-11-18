1d05ffb3e9623c5387d1c179eff0bca5bb19eab2
Ji hun <jihoonv0221@gmail.com>
2023-11-18 10:31:52 +0900

    week09_chap08 v4.8

--- week09_chap08.py
+++ week09_chap08.py
@@ -1,28 +1,8 @@
-subjects = (['python', 5],['java', 9])
+subjects = (['python', 5], ['java', 9], ['go', 7])
 subjects = dict(subjects)
 
-print(subjects)
-print(subjects['java'])
-subjects['go'] = 7  # add
-print(subjects)
-subjects['java'] = 8  # update
-print(subjects)
-
-for subject in subjects:
-    print(subject)
-
-for subject in subjects.keys():
-    print(subject)
-
-for subject in subjects.values():
-    print(subject)
-
-for subject in subjects.items():
-    print(subject)
-
-for subject, students in subjects.items():
-    print(subject, students)
-    print(f"{subject} 과목을 수강하는 학생의 인원은 {students}입니다.")
-
-del subjects['java']
-print(subjects)
+print(subjects['go'])
+print(subjects.get('go'))
+print(subjects.values())
+print(list(subjects.values()))
+print(len(subjects))
